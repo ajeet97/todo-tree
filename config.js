aa8830c24c96271146377330d2cf965aa7391e1e
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2019-04-03 17:02:14 +0530

    fix indentation

--- config.js
+++ config.js
@@ -1,113 +1,95 @@
-var vscode = require( 'vscode' );
-var fs = require( 'fs' );
-var path = require( 'path' );
+var vscode = require('vscode');
+var fs = require('fs');
+var path = require('path');
 
 var context;
 
-function init( c )
-{
-    context = c;
+function init(c) {
+	context = c;
 }
 
-function shouldGroup()
-{
-    return context.workspaceState.get( 'grouped', vscode.workspace.getConfiguration( 'todo-tree' ).get( 'grouped', false ) );
+function shouldGroup() {
+	return context.workspaceState.get('grouped', vscode.workspace.getConfiguration('todo-tree').get('grouped', false));
 }
 
-function shouldExpand()
-{
-    return context.workspaceState.get( 'expanded', vscode.workspace.getConfiguration( 'todo-tree' ).get( 'expanded', false ) );
+function shouldExpand() {
+	return context.workspaceState.get('expanded', vscode.workspace.getConfiguration('todo-tree').get('expanded', false));
 }
 
-function shouldFlatten()
-{
-    return context.workspaceState.get( 'flat', vscode.workspace.getConfiguration( 'todo-tree' ).get( 'flat', false ) );
+function shouldFlatten() {
+	return context.workspaceState.get('flat', vscode.workspace.getConfiguration('todo-tree').get('flat', false));
 }
 
-function shouldShowTagsOnly()
-{
-    return context.workspaceState.get( 'tagsOnly', vscode.workspace.getConfiguration( 'todo-tree' ).get( 'tagsOnly', false ) );
+function shouldShowTagsOnly() {
+	return context.workspaceState.get('tagsOnly', vscode.workspace.getConfiguration('todo-tree').get('tagsOnly', false));
 }
 
-function shouldShowCounts()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).get( 'showCountsInTree', false );
+function shouldShowCounts() {
+	return vscode.workspace.getConfiguration('todo-tree').get('showCountsInTree', false);
 }
 
-function shouldShowLineNumbers()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).get( 'showLineNumbersInTree', false );
+function shouldShowLineNumbers() {
+	return vscode.workspace.getConfiguration('todo-tree').get('showLineNumbersInTree', false);
 }
 
-function showFilterCaseSensitive()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).get( 'filterCaseSensitive', false );
+function showFilterCaseSensitive() {
+	return vscode.workspace.getConfiguration('todo-tree').get('filterCaseSensitive', false);
 }
 
-function isRegexCaseSensitive()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).get( 'regexCaseSensitive', true );
+function isRegexCaseSensitive() {
+	return vscode.workspace.getConfiguration('todo-tree').get('regexCaseSensitive', true);
 }
 
-function showBadges()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).get( 'showBadges', false );
+function showBadges() {
+	return vscode.workspace.getConfiguration('todo-tree').get('showBadges', false);
 }
 
-function regex()
-{
-    return {
-        tags: vscode.workspace.getConfiguration( 'todo-tree' ).get( 'tags' ),
-        regex: vscode.workspace.getConfiguration( 'todo-tree' ).get( 'regex' ),
-        caseSensitive: vscode.workspace.getConfiguration( 'todo-tree' ).get( 'regexCaseSensitive' )
-    };
+function regex() {
+	return {
+		tags: vscode.workspace.getConfiguration('todo-tree').get('tags'),
+		regex: vscode.workspace.getConfiguration('todo-tree').get('regex'),
+		caseSensitive: vscode.workspace.getConfiguration('todo-tree').get('regexCaseSensitive')
+	};
 }
 
-function ripgrepPath()
-{
-    function exeName()
-    {
-        var isWin = /^win/.test( process.platform );
-        return isWin ? "rg.exe" : "rg";
-    }
+function ripgrepPath() {
+	function exeName() {
+		var isWin = /^win/.test(process.platform);
+		return isWin ? "rg.exe" : "rg";
+	}
 
-    function exePathIsDefined( rgExePath )
-    {
-        return fs.existsSync( rgExePath ) ? rgExePath : undefined;
-    }
+	function exePathIsDefined(rgExePath) {
+		return fs.existsSync(rgExePath) ? rgExePath : undefined;
+	}
 
-    var rgPath = "";
+	var rgPath = "";
 
-    rgPath = exePathIsDefined( vscode.workspace.getConfiguration( 'todo-tree' ).ripgrep );
-    if( rgPath ) return rgPath;
+	rgPath = exePathIsDefined(vscode.workspace.getConfiguration('todo-tree').ripgrep);
+	if (rgPath) return rgPath;
 
-    rgPath = exePathIsDefined( path.join( path.dirname( path.dirname( require.main.filename ) ), "node_modules/vscode-ripgrep/bin/", exeName() ) );
-    if( rgPath ) return rgPath;
+	rgPath = exePathIsDefined(path.join(path.dirname(path.dirname(require.main.filename)), "node_modules/vscode-ripgrep/bin/", exeName()));
+	if (rgPath) return rgPath;
 
-    rgPath = exePathIsDefined( path.join( path.dirname( path.dirname( require.main.filename ) ), "node_modules.asar.unpacked/vscode-ripgrep/bin/", exeName() ) );
-    if( rgPath ) return rgPath;
+	rgPath = exePathIsDefined(path.join(path.dirname(path.dirname(require.main.filename)), "node_modules.asar.unpacked/vscode-ripgrep/bin/", exeName()));
+	if (rgPath) return rgPath;
 
-    return rgPath;
+	return rgPath;
 }
 
-function globs()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).globs;
+function globs() {
+	return vscode.workspace.getConfiguration('todo-tree').globs;
 }
 
-function tags()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).tags;
+function tags() {
+	return vscode.workspace.getConfiguration('todo-tree').tags;
 }
 
-function shouldSortTagsOnlyViewAlphabetically()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).sortTagsOnlyViewAlphabetically;
+function shouldSortTagsOnlyViewAlphabetically() {
+	return vscode.workspace.getConfiguration('todo-tree').sortTagsOnlyViewAlphabetically;
 }
 
-function labelFormat()
-{
-    return vscode.workspace.getConfiguration( 'todo-tree' ).labelFormat;
+function labelFormat() {
+	return vscode.workspace.getConfiguration('todo-tree').labelFormat;
 }
 
 module.exports.init = init;
--- ripgrep.js
+++ ripgrep.js
@@ -9,29 +9,26 @@
  */
 
 'use strict';
-const child_process = require( 'child_process' );
-const fs = require( 'fs' );
+const child_process = require('child_process');
+const fs = require('fs');
 
 var currentProcess;
 
-function RipgrepError( error, stderr )
-{
-    this.message = error;
-    this.stderr = stderr;
+function RipgrepError(error, stderr) {
+	this.message = error;
+	this.stderr = stderr;
 }
 
-function formatResults( stdout )
-{
-    stdout = stdout.trim();
+function formatResults(stdout) {
+	stdout = stdout.trim();
 
-    if( !stdout )
-    {
-        return [];
-    }
+	if (!stdout) {
+		return [];
+	}
 
-    return stdout
-        .split( '\n' )
-        .map( ( line ) => new Match( line ) );
+	return stdout
+		.split('\n')
+		.map((line) => new Match(line));
 }
 
 /**
@@ -43,144 +40,118 @@ function formatResults( stdout )
  * @param {Array<string>} option.globs a set of globs to include/exclude. See `-g` option
  * @param {string} [searchTerm]
  */
-module.exports.search = function ripGrep( cwd, options, searchTerm )
-{
-    // If you're invoking the function with two arguments, just the `cwd` and `searchTerm`
-    if( arguments.length === 2 && typeof options === 'string' )
-    {
-        searchTerm = options;
-        options = {};
-    }
+module.exports.search = function ripGrep(cwd, options, searchTerm) {
+	// If you're invoking the function with two arguments, just the `cwd` and `searchTerm`
+	if (arguments.length === 2 && typeof options === 'string') {
+		searchTerm = options;
+		options = {};
+	}
 
-    if( !cwd )
-    {
-        return Promise.reject( { error: 'No `cwd` provided' } );
-    }
+	if (!cwd) {
+		return Promise.reject({ error: 'No `cwd` provided' });
+	}
 
-    if( arguments.length === 1 )
-    {
-        return Promise.reject( { error: 'No search term provided' } );
-    }
+	if (arguments.length === 1) {
+		return Promise.reject({ error: 'No search term provided' });
+	}
 
-    options.regex = options.regex || '';
-    options.globs = options.globs || [];
-    options.string = searchTerm || options.string || '';
+	options.regex = options.regex || '';
+	options.globs = options.globs || [];
+	options.string = searchTerm || options.string || '';
 
-    var rgPath = options.rgPath;
-    var isWin = /^win/.test( process.platform );
+	var rgPath = options.rgPath;
+	var isWin = /^win/.test(process.platform);
 
-    if( !fs.existsSync( rgPath ) )
-    {
-        return Promise.reject( { error: "ripgrep executable not found (" + rgPath + ")" } );
-    }
-    if( !fs.existsSync( cwd ) )
-    {
-        return Promise.reject( { error: "root folder not found (" + cwd + ")" } );
-    }
+	if (!fs.existsSync(rgPath)) {
+		return Promise.reject({ error: "ripgrep executable not found (" + rgPath + ")" });
+	}
+	if (!fs.existsSync(cwd)) {
+		return Promise.reject({ error: "root folder not found (" + cwd + ")" });
+	}
 
-    if( isWin )
-    {
-        rgPath = '"' + rgPath + '"';
-    }
-    else
-    {
-        rgPath = rgPath.replace( / /g, '\\ ' );
-    }
+	if (isWin) {
+		rgPath = '"' + rgPath + '"';
+	}
+	else {
+		rgPath = rgPath.replace(/ /g, '\\ ');
+	}
 
-    let execString = rgPath + ' --no-messages --vimgrep -H --column --line-number --color never ' + options.additional;
-    if( options.regex )
-    {
-        execString = `${execString} -e ${options.regex}`;
-    }
-    else if( options.string )
-    {
-        execString = `${execString} -F ${options.string}`;
-    }
+	let execString = rgPath + ' --no-messages --vimgrep -H --column --line-number --color never ' + options.additional;
+	if (options.regex) {
+		execString = `${execString} -e ${options.regex}`;
+	}
+	else if (options.string) {
+		execString = `${execString} -F ${options.string}`;
+	}
 
-    execString = options.globs.reduce( ( command, glob ) =>
-    {
-        return `${command} -g \"${glob}\"`;
-    }, execString );
+	execString = options.globs.reduce((command, glob) => {
+		return `${command} -g \"${glob}\"`;
+	}, execString);
 
-    if( options.filename )
-    {
-        var filename = options.filename;
-        if( isWin && filename.slice( -1 ) === "\\" )
-        {
-            filename = filename.substr( 0, filename.length - 1 );
-        }
-        execString += " \"" + filename + "\"";
-    }
-    else
-    {
-        execString += " .";
-    }
+	if (options.filename) {
+		var filename = options.filename;
+		if (isWin && filename.slice(-1) === "\\") {
+			filename = filename.substr(0, filename.length - 1);
+		}
+		execString += " \"" + filename + "\"";
+	}
+	else {
+		execString += " .";
+	}
 
-    if( options.outputChannel )
-    {
-        options.outputChannel.appendLine( "Command: " + execString );
-    }
+	if (options.outputChannel) {
+		options.outputChannel.appendLine("Command: " + execString);
+	}
 
-    return new Promise( function( resolve, reject )
-    {
-        // The default for omitting maxBuffer, according to Node docs, is 200kB.
-        // We'll explicitly give that here if a custom value is not provided.
-        // Note that our options value is in KB, so we have to convert to bytes.
-        const maxBuffer = ( options.maxBuffer || 200 ) * 1024;
-        var currentProcess = child_process.exec( execString, { cwd, maxBuffer } );
-        var results = "";
+	return new Promise(function (resolve, reject) {
+		// The default for omitting maxBuffer, according to Node docs, is 200kB.
+		// We'll explicitly give that here if a custom value is not provided.
+		// Note that our options value is in KB, so we have to convert to bytes.
+		const maxBuffer = (options.maxBuffer || 200) * 1024;
+		var currentProcess = child_process.exec(execString, { cwd, maxBuffer });
+		var results = "";
 
-        currentProcess.stdout.on( 'data', function( data )
-        {
-            if( options.outputChannel )
-            {
-                options.outputChannel.appendLine( data );
-            }
-            results += data;
-        } );
+		currentProcess.stdout.on('data', function (data) {
+			if (options.outputChannel) {
+				options.outputChannel.appendLine(data);
+			}
+			results += data;
+		});
 
-        currentProcess.stderr.on( 'data', function( data )
-        {
-            if( options.outputChannel )
-            {
-                options.outputChannel.appendLine( data );
-            }
-            reject( new RipgrepError( data, "" ) );
-        } );
+		currentProcess.stderr.on('data', function (data) {
+			if (options.outputChannel) {
+				options.outputChannel.appendLine(data);
+			}
+			reject(new RipgrepError(data, ""));
+		});
 
-        currentProcess.on( 'close', function( code )
-        {
-            resolve( formatResults( results ) );
-        } );
+		currentProcess.on('close', function (code) {
+			resolve(formatResults(results));
+		});
 
-    } );
+	});
 };
 
-module.exports.kill = function()
-{
-    if( currentProcess !== undefined )
-    {
-        currentProcess.kill( 'SIGINT' );
-    }
+module.exports.kill = function () {
+	if (currentProcess !== undefined) {
+		currentProcess.kill('SIGINT');
+	}
 };
 
-class Match
-{
-    constructor( matchText )
-    {
-        this.file = "";
+class Match {
+	constructor(matchText) {
+		this.file = "";
 
-        if( matchText.length > 1 && matchText[ 1 ] === ':' )
-        {
-            this.file = matchText.substr( 0, 2 );
-            matchText = matchText.substr( 2 );
-        }
-        matchText = matchText.split( ':' );
-        this.file += matchText.shift();
-        this.line = parseInt( matchText.shift() );
-        this.column = parseInt( matchText.shift() );
-        this.match = matchText.join( ':' );
-    }
+		if (matchText.length > 1 && matchText[1] === ':') {
+			this.file = matchText.substr(0, 2);
+			matchText = matchText.substr(2);
+		}
+		matchText = matchText.split(':');
+		this.file += matchText.shift();
+		this.line = parseInt(matchText.shift());
+		this.column = parseInt(matchText.shift());
+		this.match = matchText.join(':');
+	}
 }
 
 module.exports.Match = Match;
